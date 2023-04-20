4a5e10a48bca0d6f4ab18d561cf61807fa8b7191
Игорь Кальчин <108684879+KalchIgor@users.noreply.github.com>
2023-04-20 18:10:52 +0300

    react-loader

--- src/components/Fetch/Fetch.jsx
+++ src/components/Fetch/Fetch.jsx
@@ -1,7 +1,7 @@
 const API_KEY = '34077503-5bfdb6066ba2aa1107947cc02';
 const BASE_URL = 'https://pixabay.com/api/';
 
-export const fetchImg = (searchQuery, page) => {
+export const fetchImg = (searchQuery,  page = 1) => {
   return fetch(
     `${BASE_URL}?q=${searchQuery}&page=${page}&key=${API_KEY}&image_type=photo&orientation=horizontal&per_page=12`
   ).then(response => {
