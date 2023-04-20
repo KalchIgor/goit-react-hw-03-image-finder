f3fbd2348e2701c21db0fd5e14424b53ec6fa679
Игорь Кальчин <108684879+KalchIgor@users.noreply.github.com>
2023-04-20 19:03:25 +0300

    loader

--- src/components/Gallery/Loader/Loader.jsx
+++ src/components/Gallery/Loader/Loader.jsx
@@ -1,6 +1,8 @@
-import { Vortex } from  'react-loader-spinner'
+import { Vortex } from 'react-loader-spinner';
+import { LoaderBackdrop, PendingLion } from './Loader.styled';
+import lion from 'Assets/lion-cartoon.png';
 
-
+/*
 <Vortex
   visible={true}
   height="80"
@@ -10,3 +12,21 @@ import { Vortex } from  'react-loader-spinner'
   wrapperClass="vortex-wrapper"
   colors={['red', 'green', 'blue', 'yellow', 'orange', 'purple']}
 />
+*/
+
+export const Loader = () => {
+  return (
+    <LoaderBackdrop>
+      <Vortex
+        visible={true}
+        height="80"
+        width="80"
+        ariaLabel="vortex-loading"
+        wrapperStyle={{}}
+        wrapperClass="vortex-wrapper"
+        colors={['red', 'green', 'blue', 'yellow', 'orange', 'purple']}
+/>
+      <PendingLion src={lion} alt="lion" />
+    </LoaderBackdrop>
+  );
+};
