1c156ccbb08b8bc3d5dad498fb85d94a99571cb9
Игорь Кальчин <108684879+KalchIgor@users.noreply.github.com>
2023-04-21 12:16:10 +0300

    Fetch.jsx   axios
     axios

--- src/components/Fetch/Fetch.jsx
+++ src/components/Fetch/Fetch.jsx
@@ -1,5 +1,9 @@
+//import axios from "axios";
+
 const API_KEY = '34077503-5bfdb6066ba2aa1107947cc02';
 const BASE_URL = 'https://pixabay.com/api/';
+
+//axios.defaults.baseURL = "https://pixabay.com/api";
 
 export const getFetch = (query,  page = 1) => {
   return fetch(
@@ -11,3 +15,24 @@ export const getFetch = (query,  page = 1) => {
     return response.json();
   });
 };
+
+/*
+export const getFetch = async(query, page = 1, per_page = 12) => {
+  try { const response = await axios.get("/", {
+        params: {
+            page,
+            per_page,
+            key: KEY,
+            q: query,
+            image_type: 'photo',
+            orientation: 'horizontal',
+        }
+    })
+    return response.data;
+  }
+  catch (error) {
+      console.log({ error });
+    } 
+   
+}
+*/
